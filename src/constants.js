81708a7856027fd90062d477797c2205c57b53eb
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-04-01 18:56:36 -0500

    feat: Completed project

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const MAX_RETRY_COUNT = 5;
+
+module.exports = {
+  MAX_RETRY_COUNT,
+};
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const term = require('terminal-kit').terminal;
+
+const { MAX_RETRY_COUNT } = require('./constants');
+const { wordService } = require('./word.machine');
+
+function formatForStdout(definitionsObj) {
+  const { word, wordResults } = definitionsObj;
+  const capitalizedWord = word.replace(word[0], word[0].toLocaleUpperCase());
+
+  term.bold.green(`\n${capitalizedWord}\n`);
+  term.table([['Definition', 'Type', 'Synonyms'], ...wordResults], {
+    hasBorder: true,
+    fit: true,
+    width: term.width,
+    borderChars: 'lightRounded',
+  });
+}
+
+function filterDefinitionData(definitionData) {
+  const {
+    word,
+    results = {
+      definition: 'No definition',
+      partOfSpeech: 'No type',
+      synonyms: 'No synonyms',
+    },
+  } = definitionData;
+
+  const desiredResults = Array.isArray(results) ? results : [results];
+  const filteredData = desiredResults.map(({ definition, partOfSpeech, synonyms }) => {
+    const synonymsToReturn = Array.isArray(synonyms) ? synonyms.join(', ') : synonyms;
+    return [definition, partOfSpeech, synonymsToReturn];
+  });
+
+  return { wordResults: filteredData, word };
+}
+
+wordService.onTransition(state => {
+  const currentState = state.value;
+
+  const { retries, results, word } = state.context;
+
+  if (retries >= MAX_RETRY_COUNT && currentState === 'rejected') {
+    console.error('Sorry something went wrong, could not get word definition 😭');
+  } else if (currentState === 'resolved') {
+    formatForStdout(filterDefinitionData({ results, word }));
+  }
+});
+
+wordService.start();
+wordService.send({ type: 'fetch' });
